f0ad48c1df5f7cd6b5890deff7562a34a6c1bbd8
resetes12 <diploporigon@gmail.com>
2025-10-10 20:04:22 +0200

    Fixed Kindler Frontier class -> Firebreather Frontier class

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -157,7 +157,7 @@
 #define FACILITY_CLASS_POKEMANIAC            0x17
 #define FACILITY_CLASS_MAGMA_GRUNT_F         0x18
 #define FACILITY_CLASS_GUITARIST             0x19
-#define FACILITY_CLASS_SUPER_NERD               0x1a
+#define FACILITY_CLASS_FIREBREATHER          0x1a
 #define FACILITY_CLASS_CAMPER                0x1b
 #define FACILITY_CLASS_PICNICKER             0x1c
 #define FACILITY_CLASS_BUG_MANIAC            0x1d
